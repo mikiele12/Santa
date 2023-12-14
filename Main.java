f233efb3b95523419fdfe981bdf00dd35c80551d
mikiele12 <144146911+mikiele12@users.noreply.github.com>
2023-12-14 11:11:34 -0500

    fixed!

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("I like to write my own stories. Y/N");
+		String q1 = in.nextLine().toUpperCase();
+		System.out.println("i like to take picture.");
+		String q2 = in.nextLine().toUpperCase();
+		if (q1.startsWith("Y")) {
+			
+
+			
+		}else {
+			System.out.println("it's a no");
+		}
+
+	}
+
+}
---
+++ src/santa/Main.java
@@ -0,0 +1,24 @@
+package santa;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner in= new Scanner(System.in);
+		System.out.println("entre your name.");
+		String name= in.nextLine();
+		System.out.println("what date is christmas");
+		int date = in.nextInt();
+		System.out.println("what do you like 4 things for christmas?");
+		String item1= in.nextLine();
+		String item2= in.nextLine();
+		String item3= in.nextLine();
+		String item4= in.nextLine();
+		System.out.println("Dear, Santa I have been good for everyone "
+				+ "until now. So, Santa I am asking you today to give me " 
+				+ item1  + ", " + item2 + ", " +  item3 + " and "  +  
+				item4 + " for christams. THank you! from " + name + " ");
+	}
+
+}
